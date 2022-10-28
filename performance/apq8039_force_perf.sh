43eb1efc9f590326c71f254ba5c7fbd2a7b4278f
Chi Ma <chi@squareup.com>
2022-10-28 01:43:03 -0700

    test v7

---
+++ performance/apq8039_force_perf.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1862173a559eddbd1f7f88b0dcfd66fb0944464a2956fbda60ee00d57c2f1505
+size 388
---
+++ performance/sdm660_disable_therm_force_perf.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5857bbcb39fb8a327ba94fb27ef4b49cb8933960a9b2ceabfcbabedfea23b3f1
+size 577
